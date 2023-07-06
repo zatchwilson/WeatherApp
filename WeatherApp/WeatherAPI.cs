0ec16bdd2d719053015ed73cb70b2d38dd7ee735
zatchwilson <zachthewilson@gmail.com>
2023-07-05 20:41:10 -0600

    Removed APIKey from public code.

---
+++ WeatherApp/WeatherAPI.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using static System.Net.WebRequestMethods;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace WeatherApp
+{
+    public class WeatherAPI
+    {
+        public WeatherData StartClient(string zipCode)
+        {
+            string apiKey = ConfigurationManager.AppSettings.Get("APIKey");
+            string rnUri = "https://weatherapi-com.p.rapidapi.com/current.json?q=" + zipCode;
+            WeatherData data = null;
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(rnUri),
+                Headers =
+                {
+                    { "X-RapidAPI-Key", apiKey },
+                    { "X-RapidAPI-Host", "weatherapi-com.p.rapidapi.com" },
+                },
+            };
+
+            data = TestClient(client, request, data).Result;
+
+            return data;
+        }
+
+
+        static async Task<WeatherData> TestClient(HttpClient client, HttpRequestMessage request, WeatherData data)
+        {
+            try
+            {
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    data = JsonConvert.DeserializeObject<WeatherData>(body);
+                }
+            }
+
+            catch
+            {
+                Console.WriteLine("The information that was input is not valid.");
+            }
+
+            return data;
+        }
+
+        public void FinishedWithConnection(string json)
+        {
+
+        }
+
+        public WeatherData ParseJson(string json)
+        {
+            WeatherData data = JsonConvert.DeserializeObject<WeatherData>(json);
+            return data;
+        }
+    }
+}
+
+
